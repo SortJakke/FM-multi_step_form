2913bb9ac461a3c2eeaac5b2af933b51df650540
Sortjakke <erick.matheusrs1003@gmail.com>
2025-10-22 15:44:23 -0300

    feat: implement MultiStepForm component to control navigation between steps

--- src/pages/MultiStepForm.tsx
+++ src/pages/MultiStepForm.tsx
@@ -1,5 +1,49 @@
+import { useFormContext } from "../context/useFormContext"
+import StepPersonalInfo from "../components/StepPersonalInfo"
+import StepPlanSelection from "../components/StepPlanSelection"
+import StepAddOns from "../components/StepAddOns"
+import StepConfirmation from "../components/StepConfirmation"
+
 function MultiStepForm() {
-  return <div>Multi Step Form</div>
+  const { currentStep } = useFormContext()
+
+  const renderStep = () => {
+    switch (currentStep) {
+      case 0:
+        return <StepPersonalInfo/>
+      case 1:
+        return <StepPlanSelection/>
+      case 2:
+        return <StepAddOns/>
+      case 3:
+        return <StepConfirmation/>
+      default:
+        return <div>Unknown Step</div>
+    }
+  }
+
+  const stepTitles = ["Your Info", "Select Plan", "Add-Ons", "Summary"]
+
+  return (
+    <div className="w-full p-4 bg-purple-600 grid gap-4">
+      <div className="w-fit p-4 mx-auto text-blue-100 flex items-center gap-4">
+        {stepTitles.map((title, index) => (
+          <div
+            key={index}
+            aria-label={title}
+            className={`w-8 h-8 rounded-full flex items-center justify-center font-semibold border ${
+              currentStep === index
+                ? " border-blue-200 bg-blue-200 text-blue-950"
+                : " bg-purple-600"
+            }`}
+          >
+            {index + 1}
+          </div>
+        ))}
+      </div>
+      {renderStep()}
+    </div>
+  )
 }
 
 export default MultiStepForm
