ac3c7fcf296e237b808820d88fcabed96c602274
Sortjakke <erick.matheusrs1003@gmail.com>
2025-10-27 10:51:29 -0300

    feat: add validations and error messages to the personal information form

--- src/components/StepPersonalInfo.tsx
+++ src/components/StepPersonalInfo.tsx
@@ -1,22 +1,35 @@
 import { useFormContext } from "../context/useFormContext"
+import { useForm } from "react-hook-form"
+
+type PersonalInfoFormData = {
+  name: string
+  email: string
+  phone: string
+}
 
 function StepPersonalInfo() {
   const { formData, setFormData, nextStep } = useFormContext()
 
-  const handleInputChange =
-    (field: keyof typeof formData) =>
-    (e: React.ChangeEvent<HTMLInputElement>) => {
-      setFormData({ [field]: e.target.value })
-    }
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<PersonalInfoFormData>({
+    defaultValues: {
+      name: formData.name,
+      email: formData.email,
+      phone: formData.phone,
+    },
+  })
 
-  const handleSubmit = (e: React.FormEvent) => {
-    e.preventDefault()
+  const onSubmit = (e: PersonalInfoFormData) => {
+    setFormData(e)
     nextStep()
   }
 
   return (
     <form
-      onSubmit={handleSubmit}
+      onSubmit={handleSubmit(onSubmit)}
       className="h-full flex flex-col justify-between gap-4 text-blue-950"
     >
       <div className="flex flex-col gap-4 rounded-md bg-white p-6">
@@ -25,40 +38,74 @@ function StepPersonalInfo() {
           Please provide your name, email address, and phone number.
         </p>
         <label className="flex flex-col text-sm">
-          Name
+          <div className="flex justify-between items-center">
+            Name
+            {errors.name && (
+              <span className="text-red-500 text-xs font-semibold">
+                {errors.name.message}
+              </span>
+            )}
+          </div>
           <input
             type="text"
-            name="name"
-            value={formData.name}
-            onChange={handleInputChange("name")}
-            required
-            className="placeholder-gray-400 placeholder:font-medium border border-gray-400 py-2 px-4 rounded"
+            {...register("name", { required: "This field is required" })}
+            className={`placeholder-gray-400 placeholder:font-medium border border-gray-400 py-2 px-4 rounded ${
+              errors.name ? "border-red-500" : ""
+            }`}
             placeholder="e.g. Stephen King"
           />
         </label>
 
         <label className="flex flex-col text-sm">
-          Email Address
+          <div className="flex justify-between items-center">
+            Email Address
+            {errors.email && (
+              <span className="text-red-500 text-xs font-semibold">
+                {errors.email.message}
+              </span>
+            )}
+          </div>
           <input
             type="email"
-            name="email"
-            value={formData.email}
-            onChange={handleInputChange("email")}
-            required
-            className="placeholder-gray-400 placeholder:font-medium border border-gray-400 py-2 px-4 rounded"
+            {...register("email", {
+              required: "This field is required",
+              pattern: {
+                value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+                message: "Invalid email",
+              },
+            })}
+            className={`placeholder-gray-400 placeholder:font-medium border border-gray-400 py-2 px-4 rounded ${
+              errors.email ? "border-red-500" : ""
+            }`}
             placeholder="e.g. Stephenking@lorem.com"
           />
         </label>
 
         <label className="flex flex-col text-sm">
-          Phone Number
+          <div className="flex justify-between items-center">
+            Phone Number
+            {errors.phone && (
+              <span className="text-red-500 text-xs font-semibold">
+                {errors.phone.message}
+              </span>
+            )}
+          </div>
           <input
             type="tel"
-            name="phone"
-            value={formData.phone}
-            onChange={handleInputChange("phone")}
-            required
-            className="placeholder-gray-400 placeholder:font-medium border border-gray-400 py-2 px-4 rounded"
+            {...register("phone", {
+              required: "This field is required",
+              pattern: {
+                value: /^[\d\s()-]+$/,
+                message: "Enter only numbers",
+              },
+              minLength: {
+                value: 8,
+                message: "Very short phone number",
+              },
+            })}
+            className={`placeholder-gray-400 placeholder:font-medium border border-gray-400 py-2 px-4 rounded ${
+              errors.phone ? "border-red-500" : ""
+            }`}
             placeholder="e.g. +1 234 567 890"
           />
         </label>
